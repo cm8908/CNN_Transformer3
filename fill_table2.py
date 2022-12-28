903a75f12e7ed8b1b45c891155282c79046a5044
cm8908 <jungminseob2@gmail.com>
2022-12-28 08:16:26 +0000

    minor fix

--- fill_table2.py
+++ fill_table2.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import time
-import nvidia_smi
+# import nvidia_smi
 import pandas as pd
 from tqdm import tqdm
 from argparse import ArgumentParser
@@ -12,8 +12,12 @@ def get_model(args, is_train=False):
     else:
         from model_search import TSP_net
     embedding = args.embedding
-    nb_neighbors = 10
-    kernel_size = nb_neighbors + 1
+    if embedding == 'conv':
+        nb_neighbors = 10
+        kernel_size = nb_neighbors + 1
+    else:
+        nb_neighbors = None
+        kernel_size = None
     segm_len = args.segm_len
     batchnorm = True
     return TSP_net(embedding, nb_neighbors, kernel_size, dim_input_nodes=2, dim_emb=128, dim_ff=512,
@@ -35,56 +39,59 @@ def main(args):
     # Get Train Model and Measure T-Time
     model = get_model(args, is_train=True).to(device)
     t_time_s = time.time()
-    if args.bsz is None:
+    if args.train_bsz is None:
         model(data_10k, deterministic=False)
     else:
-        assert 10_000 % args.bsz == 0
-        for i in tqdm(range(0, 10_000, args.bsz)):
-            minibatch = data_10k[i:i+args.bsz]
+        assert 10_000 % args.train_bsz == 0
+        for i in tqdm(range(0, 10_000, args.train_bsz)):
+            minibatch = data_10k[i:i+args.train_bsz]
             model(minibatch, deterministic=False)
     t_time = time.time() - t_time_s
     result_dict['T-Time (total)'] = t_time
     print('T-Time (total):', t_time)
-    t_time = t_time / 10_000 if args.bsz is not None else t_time
+    t_time = t_time / 10_000 if args.train_bsz is not None else t_time
     result_dict['T-Time (per instance)'] = t_time
     print('T-Time (per instance):', t_time)
+    torch.cuda.empty_cache()
 
     # Measure Memory Usage
-    nvidia_smi.nvmlInit()
-    handle = nvidia_smi.nvmlDeviceGetHandleByIndex(args.gpu_id)
-    info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
-    result_dict['Memory Usage'] = info.used
-    print('Memory Usage:', info.used)
-    nvidia_smi.nvmlShutdown()
-    torch.cuda.empty_cache()
+    # nvidia_smi.nvmlInit()
+    # handle = nvidia_smi.nvmlDeviceGetHandleByIndex(args.gpu_id)
+    # info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+    # result_dict['Memory Usage'] = info.used
+    # print('Memory Usage:', info.used)
+    # nvidia_smi.nvmlShutdown()
+    # torch.cuda.empty_cache()
 
     # Measure I Time Greedy
     model_infer = get_model(args, is_train=False).to(device)
-    if args.bsz is not None:
-        i_time_s = time.time()
-        for i in tqdm(range(0, args.nb_instances_eval, args.bsz)):
-            minibatch = data_10k[i:i+args.bsz]
-            model_infer(minibatch, greedy=True, beamsearch=False, B=1)
-        i_time_g = time.time() - i_time_s
-        result_dict['I-Time Greedy (total)'] = i_time_g
-        print('I-Time Greedy (total):', i_time_g)
-        i_time_g /= args.nb_instances_eval
-        result_dict['I-Time Greedy (per instance)'] = i_time_g
-        print('I-Time Greedy (per instance):', i_time_g)
-        torch.cuda.empty_cache()
+    model_infer.eval()
+    with torch.no_grad():
+        if args.greedy_bsz is not None:
+            i_time_s = time.time()
+            for i in tqdm(range(0, args.nb_instances_eval, args.greedy_bsz)):
+                minibatch = data_10k[i:i+args.greedy_bsz]
+                model_infer(minibatch, greedy=True, beamsearch=False, B=1)
+            i_time_g = time.time() - i_time_s
+            result_dict['I-Time Greedy (total)'] = i_time_g
+            print('I-Time Greedy (total):', i_time_g)
+            i_time_g /= args.nb_instances_eval
+            result_dict['I-Time Greedy (per instance)'] = i_time_g
+            print('I-Time Greedy (per instance):', i_time_g)
+            torch.cuda.empty_cache()
     
-    # Measure I Time Beamsearch
-    if args.bsz is not None:
-        i_time_s = time.time()
-        for i in tqdm(range(0, args.nb_instances_eval, args.bsz)):
-            minibatch = data_10k[i:i+args.bsz]
-            model_infer(minibatch, greedy=False, beamsearch=True, B=2500)
-        i_time_bs = time.time() - i_time_s
-        result_dict['I-Time Beamsearch (total)'] = i_time_bs
-        print('I-Time Beamsearch (total):', i_time_bs)
-        i_time_bs /= args.nb_instances_eval
-        result_dict['I-Time Beamsearch (per instance)'] = i_time_bs
-        print('I-Time Beamsearch (per instance):', i_time_bs)
+        # Measure I Time Beamsearch
+        if args.beamsearch_bsz is not None:
+            i_time_s = time.time()
+            for i in tqdm(range(0, args.nb_instances_eval, args.beamsearch_bsz)):
+                minibatch = data_10k[i:i+args.beamsearch_bsz]
+                model_infer(minibatch, greedy=False, beamsearch=True, B=2500)
+            i_time_bs = time.time() - i_time_s
+            result_dict['I-Time Beamsearch (total)'] = i_time_bs
+            print('I-Time Beamsearch (total):', i_time_bs)
+            i_time_bs /= args.nb_instances_eval
+            result_dict['I-Time Beamsearch (per instance)'] = i_time_bs
+            print('I-Time Beamsearch (per instance):', i_time_bs)
     
     # Write to csv
     csv_file = 'table2_results.csv'
@@ -105,7 +112,9 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--segm_len', type=int, default=None)
     parser.add_argument('--embedding', type=str, choices=['conv', 'linear'], required=True)
     parser.add_argument('--gpu_id', type=int)
-    parser.add_argument('--bsz', type=int, default=None)
+    parser.add_argument('--train_bsz', type=int, default=None)
+    parser.add_argument('--greedy_bsz', type=int, default=None)
+    parser.add_argument('--beamsearch_bsz', type=int, default=None)
     parser.add_argument('--nb_instances_eval', type=int, default=10_000)
     args = parser.parse_args()
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu_id)
